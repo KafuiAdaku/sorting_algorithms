d16d3447c5a8ce1dce29dcb233daaa0f23d90c1b
ifiok1equere <equereifiok1@gmail.com>
2023-07-25 23:00:49 +0100

    Changes to Selection function and print helper functions

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,38 @@
 #include "sort.h"
 
 /**
- * selection_sort- sorts an array of integers in ascending order using
- *		   the Selection sort algorithm
- * @array: pointer to the array to be sorted of type int
- * @size: size of the array (int).
- * Return: Nothing
+ * selection_sort - function sorts an array of integers
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ *
+ * Return: void
  */
+
 void selection_sort(int *array, size_t size)
 {
-	int i, j, min, tmp;
+	size_t i, j, min_index, temp;
+	int print_signal;
 
-	if (array == NULL || size < 2)
+	if (size < 2 || array == NULL)
 		return;
 
-	for (i = 0; i < (int)size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
-		for (j = i + 1; j < (int)size; j++)
+		print_signal = 0;
+		min_index = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+				print_signal = 1;
+			}
 		}
-		if (min != i)
-		{
-			tmp = array[i];
-			array[i] = array[min];
-			array[min] = tmp;
-		}
-		print_array(array, size);
+		temp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = temp;
+		if (print_signal == 1)
+			print_array(array, size);
 	}
 }
