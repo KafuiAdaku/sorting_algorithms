616408039a293ea8a37f2fcccdf03a54047507a5
ifiok1equere <equereifiok1@gmail.com>
2023-07-26 00:47:32 +0100

    Changes for betty compliance

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,10 +11,10 @@
 
 void swap(int *array, size_t index1, size_t index2)
 {
-        int temp = array[index1];
+	int temp = array[index1];
 
-        array[index1] = array[index2];
-        array[index2] = temp;
+	array[index1] = array[index2];
+	array[index2] = temp;
 }
 
 /**
